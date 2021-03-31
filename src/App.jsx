555b600469f5edb998a2de34b7552e6575337b85
miltoncodeyt <miltoncodeyt@gmail.com>
2021-03-30 21:34:42 -0500

    Updated: layout and distribution

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./App.scss";
+
+const App = () => {
+  return (
+    <main className="Main">
+      <section className="Header">Header</section>
+      <section className="SectionDays">SectionDays</section>
+      <section className="SectionVisit">SectionVisit</section>
+      <section className="SectionTop">SectionTop</section>
+      <section className="SectionAside">SectionAside</section>
+    </main>
+  );
+};
+
+export default App;
